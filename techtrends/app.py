140ffe766e5212642d2b90579e77828ee31e933a
Amer Osmanović <oamerpg@gmail.com>
2022-12-03 13:08:14 +0100

    logging changes on app.py

--- techtrends/app.py
+++ techtrends/app.py
@@ -5,15 +5,16 @@ import time
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
-connection_count = 0
-
 def logger_message(message):
     date = time.asctime()
     return f"{date}, {message}"
 
-# Function to get a database connection.
-# This function connects to database with the name `database.db`
+
 def get_db_connection():
+    """
+    Function to get a database connection.
+    This function connects to database with the name `database.db`
+    """
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     global connection_count
@@ -30,7 +31,9 @@ def get_post(post_id):
 
 # Define the Flask application
 app = Flask(__name__)
+app.config['connection_count'] = 0
 app.config['SECRET_KEY'] = 'your secret key'
+
 
 @app.route('/healthz')
 def healthz():
@@ -68,13 +71,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.error("There are no articles! ")
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info("Article \'%s' ")
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("About page successfully retrived")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -97,6 +103,35 @@ def create():
 
     return render_template('create.html')
 
-# start the application on port 3111
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    """
+    Starting the application on port 3111
+    Recording the logs to both STDOUT & STDERR
+    """
+    logger = logging.getLogger('logfile')
+    logger.setLevel(logging.DEBUG)
+
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stderror_handler = logging.StreamHandler(stream=sys.stderr)
+    stderror_handler.setLevel(logging.ERROR)
+
+    formatter = logging.Formatter('%(asctime)s __ %(name)s __ %(levelname)s __ %(message)s',datefmt='%d/%m/%Y')
+
+    stdout_handler.setFormatter(formatter)
+    stderror_handler.setFormatter(formatter)
+
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderror_handler)
+
+    handlers = [stdout_handler, stderror_handler]
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filemode= 'w',
+        format = '%(levelname)s __ %(asctime)s __ %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=handlers
+    )
+    app.run(host='0.0.0.0', port='3111')
